9783c09fe2bb1ee16daf985fa3454cf95328d3c0
David Boyne <davidboyne@Davids-MBP.lan>
2022-01-13 14:41:56 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.yml$/i,
+      loader: 'raw-loader',
+    });
+    // Important: return the modified config
+    return config;
+  },
 }
